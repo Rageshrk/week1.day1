0b64f44596f7384d830383b70b03e613a3ccce07
Rageshrk <98250419+Rageshrk@users.noreply.github.com>
2022-01-23 12:03:08 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,11 @@
+package New;
+
+public class Car {
+	short regNumber = 1234;
+	int insuranceNumber = 789032251;
+	long chasisNumber = 12334564L;
+	byte fuelCapacity = 25;
+	float engineOilCapacity = 5.2F;
+	boolean isTirePuncture = false;
+	
+}
